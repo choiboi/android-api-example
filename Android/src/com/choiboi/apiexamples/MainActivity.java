ee24569622aebb645cfad0d6ab6d1424696c54ad
choiboi <james.choiboi@gmail.com>
2013-07-31 00:15:55 -0400

    Created listview for main activity.

--- Android/src/com/choiboi/apiexamples/MainActivity.java
+++ Android/src/com/choiboi/apiexamples/MainActivity.java
@@ -2,12 +2,33 @@ package com.choiboi.apiexamples;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+    
+    private final String[] LIST = new String[]{ "Toast" };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        ListView listView = (ListView)findViewById(R.id.main_listview);
+        
+        ArrayAdapter<String> adapter =
+                new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, LIST);
+        listView.setAdapter(adapter);
+        
+        listView.setOnItemClickListener(new OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(), ((TextView) view).getText(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
