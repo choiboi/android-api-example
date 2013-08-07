2a516b8889c0bd1f2446e19b903cde835f4c469e
choiboi <james.choiboi@gmail.com>
2013-08-07 01:30:53 -0400

    Initial gallery intent files.

--- Android/src/com/choiboi/apiexamples/MainActivity.java
+++ Android/src/com/choiboi/apiexamples/MainActivity.java
@@ -14,7 +14,7 @@ import com.choiboi.apiexamples.toast.ToastMainActivity;
 
 public class MainActivity extends Activity {
     
-    private final String[] LIST = new String[]{ "Toast", "Camera" };
+    private final String[] LIST = new String[]{ "Toast", "Camera", "Gallery" };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ Android/src/com/choiboi/apiexamples/gallery/GalleryMainActivity.java
@@ -0,0 +1,7 @@
+package com.choiboi.apiexamples.gallery;
+
+import android.app.Activity;
+
+public class GalleryMainActivity extends Activity {
+    
+}
